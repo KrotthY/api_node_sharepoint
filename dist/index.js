cc90410fd49367bf1d3345c8b0dc45f441f7cbda
Carlos Vidal <Carlospatriciovidal.C@gmail.com>
2025-04-10 12:53:58 -0400

    Delete dist directory

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app.js"));
-_app["default"].listen(_app["default"].get('port'));
